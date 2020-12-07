4e9fc1a96e614a762b9e10268b51c2c3d843a4d5
Larissa S <39869560+larissa39@users.noreply.github.com>
2020-12-07 01:17:47 -0700

    Update and rename Lab2 to Lab 2 locations.js

---
+++ Lab 2 locations.js
@@ -0,0 +1,89 @@
+var locations = {
+		"type" : "FeatureCollection",
+        "features" : [
+	
+            {"type":"Feature", 
+            "properties":{
+                "name":"Genki Sushi",
+                "popupContent":"<b>Genki Suski</b><br> - a cheap and good conveyor belt sushi resturant",
+				"img": "<img src= IMG_4246.jpg>"},
+                "geometry":{
+                    "type":"Point",
+                    "coordinates":[35.66036651018742, 139.69940115950655]}},
+            
+            {"type": "Feature",
+            "properties": {
+                "name": "Adores Shibuya",
+                "popupContent":"<b>Adores Shibuya</b><br> - video acrade with fun photobooths for pictures",
+				"img": "<img src= IMG_4602.jpg>"},
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [35.66121543467788, 139.69821374382948]}},
+					
+            {"type": "Feature",
+            "properties": {
+                "name": "Cat Cafe Mocha",
+                "popupContent":"<b>Cat Cafe Mocha</b><br> - cat cafe for furry snuggles",
+				"img": "<img src= IMG_7625.jpg>"},
+                "geometry": {
+                    "type": "Point",
+		"coordinates": [35.66105455076822, 139.6973820170712]}};
+
+            {"type":"Feature", 
+            "properties":{
+                "name":"Hotel Lumiere",
+                "popupContent":"<b>Hotel Lumiere</b><br> - a cute hotel with three beds (which was hard to find!) for my mom, sister, and I",
+				"img":"<img src= IMG_3728.jpg>"},
+                "geometry":{
+                    "type":"Point",
+                    "coordinates":[35.66239697807741, 139.86133579435545]}},
+
+            {"type":"Feature", 
+            "properties":{
+                "name":"Mos Burger",
+                "popupContent":"<b>Mos Burger</b><br> - a tasty Japanese burger chain",
+				"img": "<img src= IMG_4246.jpg>"},
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [35.66377824212148, 139.86149918469388]}},
+          
+
+            {"type": "Feature",
+            "properties": {
+                "name": "Sanrio Puroland",
+                "popupContent":"<b>Sanrio Puroland</b><br> - a Sanrio theme park. Everything is ~so kawaii~",
+				"img":"<img src= 7718097168_IMG_1190.jpg>"}, 
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [35.62465762208486, 139.42953915767993]}},
+		    
+            
+            {"type": "Feature",
+            "properties": {
+                "name": "Beard Papa Harajuku",
+                "popupContent":"<b>Beard Papa Harajuku</b><br> - a creme puff shop in Harajuku, good snack for after lots of shopping",
+				"img": "<img src= https://eatoeb.com/wp-content/uploads/2018/10/Menu-Page-Header-e1574096414372.jpg>"},
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [35.671440953956015, 139.704023548999]}}, 
+				
+			
+            {"type": "Feature",
+            "properties": {
+                "name": "Tokyo Skytree",
+                "popupContent": "<b>Tokyo Skytree</b><br> - a radio broadcast tower with a shopping mall underneath",
+				"img": "<img src= 7583920800_IMG_1523.jpg"},
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [35.709990035416496, 139.81073641017545]}}]}
+				
+			
+            {"type": "Feature",
+            "properties": {
+                "name": "Senso Temple",
+                "popupContent": "<b>Senso Temple</b><br> a very aesthetically pleasing and serene Buddhist temple",
+				"img": "<img src= 68E0EA3D-7D30-42F4-8E55-3C5E462350F8.jpg>"},
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [35.714758562738545, 139.796806493158]}}]};
+            
